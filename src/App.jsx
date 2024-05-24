d35ff7e187a2991278a7fb7b891b4de1a108d59e
Ayanabha Misra <misrarimbo@gmail.com>
2024-05-24 14:51:12 +0530

    qa

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Ah Yo ... This is dev branch</h1>
+      <h1>Ah Yo ... This is qa branch</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
